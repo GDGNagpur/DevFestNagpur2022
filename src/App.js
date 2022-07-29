89cf942ddc2cb4b535b3e3d02f003ac59b40af6a
Raghav Agrawal <bt19cse044@iiitn.ac.in>
2022-07-29 23:36:55 +0530

    Template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>HELLO WORLD</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
