25f5054a87eeedfc664b3a0fe47fcda7879c5b2a
Raghav Agrawal <bt19cse044@iiitn.ac.in>
2022-07-29 23:52:20 +0530

    Base

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>HELLO WORLD</p>
+        <p>HELLO DEVELOPERS</p>
       </header>
     </div>
   );
