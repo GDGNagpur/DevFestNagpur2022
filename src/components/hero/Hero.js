11cad0fe83961d291c7a32c02314f5c096bafb47
Bhavesh <bhic2030@gmail.com>
2022-08-03 13:34:15 +0530

    website v1 complete

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -9,10 +9,16 @@ import { ReactComponent as MonumentSvgGreen } from "../../assets/hero/illustrati
 import { ReactComponent as BridgeSvg } from "../../assets/hero/illustration/bridge.svg";
 import { ReactComponent as SunSvg } from "../../assets/hero/sun.svg";
 import { ReactComponent as MoonSvg } from "../../assets/hero/moon.svg";
+import { ReactComponent as TwitterSvg } from "../../assets/hero/twitter.svg";
+import { ReactComponent as InstagramSvg } from "../../assets/hero/instagram.svg";
+import { ReactComponent as CutSvg } from "../../assets/hero/cut.svg";
+import { ReactComponent as IllustrationLightMobileBg } from "../../assets/hero/mobile-illustration-light-mode.svg";
+import { ReactComponent as IllustrationDarkMobileBg } from "../../assets/hero/mobile-illustration-dark-mode.svg";
 import gdgDarkLogo from "../../assets/hero/gdg-dark-logo.png";
 import gdgLightLogo from "../../assets/hero/gdg-light-logo.png";
 import styles from "./Hero.module.css";
 import { useThemeContext } from "../../theme/ThemeProvider";
+import CountDown from "../countdown/CountDown";
 
 const Hero = () => {
   const { theme, changeTheme } = useThemeContext();
@@ -39,13 +45,64 @@ const Hero = () => {
           </div>
         </div>
         <div className={styles["heading"]}>
-          <h1>
-            <span>Central</span> India's Largest Developer Conference
+          <h1
+            style={{
+              color:
+                theme === "light"
+                  ? "rgba(97, 97, 97, 1)"
+                  : "rgba(232, 234, 237, 1)",
+            }}
+          >
+            <p>
+              Central{" "}
+              <span className={styles["cut-mask"]}>
+                <CutSvg></CutSvg>
+              </span>
+            </p>{" "}
+            India's Largest
+          </h1>
+          <h1
+            style={{
+              color: theme === "light" ? "black" : "rgba(255, 255, 255, 1)",
+            }}
+          >
+            Developer Conference
           </h1>
         </div>
-        <div className={styles["counter"]}>30 Hours : 10 mins: 15 Sec</div>
-        <div className={styles["timeline"]}></div>
-        <div className={styles["social"]}></div>
+        <div
+          style={{
+            color: theme === "light" ? "#202124" : "#E8EAED",
+          }}
+          className={styles["counter"]}
+        >
+          <CountDown></CountDown>
+        </div>
+        <div className={styles["timeline-social"]}>
+          <div className={styles["timeline"]}>
+            <h3>5-6 November</h3>
+            <p>#Staytuned</p>
+          </div>
+          <div className={styles["social"]}>
+            <a
+              className={
+                theme === "light"
+                  ? `${styles["light-link"]}`
+                  : `${styles["dark-link"]}`
+              }
+              href="/"
+            >
+              <TwitterSvg></TwitterSvg>
+            </a>
+            <a
+              className={
+                theme === "light" ? styles["light-link"] : styles["dark-link"]
+              }
+              href="/"
+            >
+              <InstagramSvg></InstagramSvg>
+            </a>
+          </div>
+        </div>
       </div>
       <div className={styles["toggle"]}>
         <button
@@ -56,7 +113,7 @@ const Hero = () => {
               <SunSvg />
             </span>
           ) : (
-            <span className={styles["moon"]} >
+            <span className={styles["moon"]}>
               <MoonSvg />
             </span>
           )}
@@ -82,6 +139,15 @@ const Hero = () => {
         </div>
         <div className={styles["bridge"]}>
           <BridgeSvg></BridgeSvg>
+        </div>
+      </div>
+      <div className={styles["mobile-illustration-container"]}>
+        <div className={styles["mobile-illustration"]}>
+          {theme === "light" ? (
+            <IllustrationLightMobileBg></IllustrationLightMobileBg>
+          ) : (
+            <IllustrationDarkMobileBg></IllustrationDarkMobileBg>
+          )}
         </div>
       </div>
     </div>
