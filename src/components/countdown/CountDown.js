ee18f4d68a0d0efa805fca96a019b31854a333e7
raghav1701 <54554575+raghav1701@users.noreply.github.com>
2022-08-08 21:11:31 +0530

    merge (#14)
    * Create SECURITY.md
    
    * add new countdown timer
    
    Co-authored-by: Prathamesh Dukare <Prathameshdukaremail@gmail.com>

--- src/components/countdown/CountDown.js
+++ src/components/countdown/CountDown.js
@@ -5,39 +5,49 @@ import Countdown from "react-countdown";
 const CountDown = () => {
     // Random component
     const Completionist = () => <h3>We are live now !</h3>;
+    const [state, setState] = React.useState(0);
+    const [isTrue, setIsTrue] = React.useState(false);
+    const Minref = React.useRef("")
 
     // Renderer callback with condition
-    const renderer = ({days, hours, minutes, seconds, completed}) => {
+    const renderer = ({ days, hours, minutes, seconds, completed }) => {
         if (completed) {
             // Render a completed state
-            return <Completionist/>;
+            return <Completionist />;
         } else {
             // Render a countdown
-
-            const DaysString = days > 1 ? "Days" : "Day"
+            const daysString = days > 1 ? "Days" : "Day"
             const hoursString = hours > 1 ? "Hours" : "Hour";
-            const MinsString = minutes > 1 ? "Mins" : "Min";
+            const minsString = minutes > 1 ? "Minutes" : "Minute";
+            
+            // setState(minutes)
 
             return (
-                <p>
-                    <span className={styles["count"]}>{days}</span>
-                    <span>{DaysString}</span>
-                    <span className={styles["colon"]}></span>
-                    <span className={styles["count"]}>{hours}</span>
-                    <span>{hoursString}</span>
-                    <span className={styles["colon"]}></span>
-                    <span className={styles["count"]}>{minutes}</span>
-                    <span>{MinsString}</span>
-                    <span className={styles["colon"]}></span>
-                    <span className={styles["count"]}>{seconds}</span>
-                    <span>Sec</span>
-                </p>
+                <ul className={styles["countdown"]}>
+                    <li className={styles["google-blue"]}><span className={styles["days-span"]} >{days}</span>{daysString}</li>
+                    <li className={styles["google-yello"]}><span className={styles["hr-span"]} >{hours}</span>{hoursString}</li>
+                    <li className={styles["google-green"]}><span ref={Minref}>{minutes}</span>{minsString}</li>
+                    <li className={styles["google-red"]}><span className={styles["sec-span"]} >{seconds}</span>Seconds</li>
+                </ul>
+
             );
         }
     };
 
+    React.useEffect(() => {
+        if (!isTrue) {
+            setIsTrue(true)
+            return
+        } 
+        Minref.current.classList.add(styles["min-span"])
+        setTimeout(() => {
+            Minref.current.classList.remove(styles["min-span"])
+        }, 1000);
+    }
+        , [state])
+
     return (
-        <Countdown date={new Date(2022, 10, 5, 0, 0, 0, 0)} renderer={renderer}/>
+        <Countdown date={new Date(2022, 10, 5, 0, 0, 0, 0)} renderer={renderer} />
     );
 };
 
