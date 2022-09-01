65a56110aefba4fe9f3e95a6e5a24e9f4bb114ab
Bhavesh <bhic2030@gmail.com>
2022-09-01 23:39:30 +0530

    remove temp comments from App.js

--- src/App.js
+++ src/App.js
@@ -4,32 +4,34 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 import Loader from "./components/loader/Loader";
 import WeAreSpeakingLouderMain from "./components/weAreSpeakingLouderSection/WeAreSpeakingLouderMain";
+import WhatIsDevfest from "./components/whatIsDevfest/WhatIsDevfest";
 import ThingsYouDontWantToMissMain from "./components/thingsYouDontWantToMissSection/ThingsYouDontWantToMissMain";
 
 const App = () => {
   const [showPage, setShowPage] = useState(false);
 
-  // useEffect(() => {
-  //   Aos.init({ duration: 100 });
+  useEffect(() => {
+    Aos.init({ duration: 100 });
 
-  //   setTimeout(() => {
-  //     setIsLoading(false);
-  //   }, 6500);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 6500);
 
-  //   setTimeout(() => {
-  //     setShowPage(true);
-  //   }, 5000);
-  // }, [showPage]);
+    setTimeout(() => {
+      setShowPage(true);
+    }, 5000);
+  }, [showPage]);
 
-  // const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
     <div>
-      {/* {isLoading && <Loader></Loader>} */}
-      {!showPage && (
+      {isLoading && <Loader></Loader>}
+      {showPage && (
         <>
           {/* Put all sections here*/}
           <Hero></Hero>
+          <WhatIsDevfest></WhatIsDevfest>
           <WeAreSpeakingLouderMain></WeAreSpeakingLouderMain>
           <ThingsYouDontWantToMissMain></ThingsYouDontWantToMissMain>
         </>
