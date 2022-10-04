eaf7daba31c238bfea069ff170553f1aeeb21e0e
Bhavesh <bhic2030@gmail.com>
2022-10-05 00:23:17 +0530

    fix all access pass issue

--- src/components/allAccessPass/AllAccessPass.js
+++ src/components/allAccessPass/AllAccessPass.js
@@ -23,7 +23,12 @@ const AllAccessPass = () => {
           </span>
           <h3 className={styles["early-bird"]}>( Regular Pass )</h3>
         </div>
-        <a className={styles["cta-btn"]} href="https://payments.cashfree.com/forms/dfn22regular" target={"_blank"} rel="noreferrer">
+        <a
+          className={styles["cta-btn"]}
+          href="https://payments.cashfree.com/forms/dfn22regular"
+          target={"_blank"}
+          rel="noreferrer"
+        >
           Grab Yours
         </a>
       </div>
@@ -57,27 +62,6 @@ const AllAccessPass = () => {
             <HackathonSvg />
             <span>Hackathons</span>
           </div>
-          <div className={styles["perk"]}>
-            <HackathonSvg />
-            <span>Hackathons</span>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles["illustrations"]}>
-        <div
-          data-aos="fade-up"
-          data-aos-duration="1000"
-          className={styles["mmt-svg"]}
-        >
-          <WhiteMmtSvg />
-        </div>
-        <div
-          data-aos="fade-down-left"
-          data-aos-duration="1000"
-          className={styles["lion-svg"]}
-        >
-          <WhiteLionSvg />
         </div>
       </div>
 
