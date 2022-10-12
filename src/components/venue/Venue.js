889367064afbb6e0d3ed5ffda456a07db2ebef2d
Bhavesh <bhic2030@gmail.com>
2022-10-12 23:08:13 +0530

    fix venue section

--- src/components/venue/Venue.js
+++ src/components/venue/Venue.js
@@ -4,13 +4,18 @@ import styles from "./Venue.module.css";
 import Location from "../../assets/venue/Location.png";
 import MobView from "../../assets/venue/MobView.png";
 import LargeView from "../../assets/venue/LargeView.png";
+import { useColorModeValue } from "../../hooks/useColorModeValue";
 
 const Venue = () => {
   const isTabletOrMobile = useMediaQuery({ query: "(max-width: 800px)" });
 
   return (
     <div className={styles["venueContainer"]}>
-      <div id="venue" className={styles["heading"]}>Venue</div>
+      <div id="venue" className={styles["heading"]}>
+        <h2 style={{ color: useColorModeValue("#505050", "#ffffff") }}>
+          Venue of DevFest
+        </h2>
+      </div>
       <div className={styles["container"]}>
         <div className={styles["mapContainer"]}>
           <div className={styles["text"]}>
@@ -18,7 +23,9 @@ const Venue = () => {
               <img src={Location} alt="location"></img>
             </div>
             <div className={styles["locationText"]}>
-              Regenta Central Hotel & Convention Centre, Nagpur
+              <p style={{ color: useColorModeValue("#505050", "#ffffff") }}>
+                Regenta Central Hotel & Convention Centre, <br /> Nagpur
+              </p>
             </div>
           </div>
           <div className={styles["maps"]}>
@@ -26,6 +33,7 @@ const Venue = () => {
               title="map"
               loading="lazy"
               allowFullScreen
+              frameBorder={0}
               referrerPolicy="no-referrer-when-downgrade"
               src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB5jdHJlLcc_weCTCsnnFjt4s2T2viiqKs&q=230,%20Gangabai%20Ghat%20Road,%20Great%20Nag%20Rd,%20Jagnade%20Square,%20Nandanvan,%20Nagpur,%20Maharashtra%20440009"
             ></iframe>
