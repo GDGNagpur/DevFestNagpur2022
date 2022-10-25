2c0323926f29c1b1040f418890d56dd7a3ba1cf0
Raghav Agrawal <bt19cse044@iiitn.ac.in>
2022-10-25 09:59:29 +0530

    venue changes

--- src/components/venue/Venue.js
+++ src/components/venue/Venue.js
@@ -24,7 +24,7 @@ const Venue = () => {
             </div>
             <div className={styles["locationText"]}>
               <p style={{ color: useColorModeValue("#505050", "#ffffff") }}>
-                Suresh Bhat Auditorium, <br /> Nagpur
+                Kavivarya Suresh Bhat Auditorium, <br /> Nagpur
               </p>
             </div>
           </div>
