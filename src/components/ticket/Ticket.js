6ef2274c1662ebb1acc49493fe5b174af9757d5a
Bhavesh <bhic2030@gmail.com>
2022-10-30 15:31:55 +0530

    add ticket ui, and export as pdf feature

--- src/components/ticket/Ticket.js
+++ src/components/ticket/Ticket.js
@@ -1,31 +1,127 @@
-import React from 'react'
-import styles from "./Ticket.module.css"
-import gdgDarkLogo from "../../assets/hero/gdg-dark-logo.png";
-import gdgLightLogo from "../../assets/hero/gdg-light-logo.png";
-import { ReactComponent as DevfestLightSvg } from "../../assets/hero/devfest-light.svg";
-import { ReactComponent as DevfestDarkSvg } from "../../assets/hero/devfest-dark.svg";
-import {useColorModeValue} from "../../hooks/useColorModeValue"
+import React from "react";
+import styles from "./Ticket.module.css";
+import { ReactComponent as DevfestLightSvg } from "../../assets/ticket/devfest-nagpur.svg";
+import { ReactComponent as GdgNagpurSvg } from "../../assets/ticket/gdg-nagpur.svg";
+import { ReactComponent as GdgSvg } from "../../assets/ticket/gdg.svg";
+import { ReactComponent as MonumentSvg } from "../../assets/ticket/monument.svg";
+import { ReactComponent as Monument1Svg } from "../../assets/ticket/monument1.svg";
+import { ReactComponent as BridgeSvg } from "../../assets/ticket/bridge.svg";
+import { ReactComponent as PeopleSvg } from "../../assets/ticket/people.svg";
+import { ReactComponent as PassSvg } from "../../assets/ticket/pass.svg";
+import peerlist from "../../assets/sponsors/Peerlist.png";
+import stream from "../../assets/sponsors/Stream.png";
+import orkes from "../../assets/sponsors/Orkes.png";
+import moreficient from "../../assets/sponsors/Moreficient.png";
+import { useColorModeValue } from "../../hooks/useColorModeValue";
+import QRCode from "react-qr-code"
 
 const Ticket = () => {
+
   return (
     <div className={styles["container"]}>
       <div className={styles["content"]}>
-        <div className={styles["main"]}>
+        <div
+          style={{
+            boxShadow:
+              "-2px 2px 5px 0px rgba(26, 30, 38, 0.2), 0px -2px 5px 0px rgba(26, 30, 38, 0.2)",
+          }}
+          className={styles["main"]}
+        >
           <div className={styles["gdg-logo"]}>
-            <img
-              draggable={false}
-              src={useColorModeValue(gdgDarkLogo, gdgLightLogo)}
-              alt="Google Developers Groups, Nagpur"
-            />
+            <GdgNagpurSvg></GdgNagpurSvg>
           </div>
-          <div data-aos="none" className={styles["devfest-svg"]}>
-            {useColorModeValue(<DevfestDarkSvg/>, <DevfestLightSvg/>)}
+          <div className={styles["people-svg"]}>
+            <PeopleSvg></PeopleSvg>
+          </div>
+          <div className={styles["name"]}>
+            <h2>Bhavesh Chaudhari</h2>
+          </div>
+          <div className={styles["pass-id"]}>
+            <div className={styles["pass"]}>
+              <div className={styles["pass-svg"]}>
+                <PassSvg></PassSvg>
+              </div>
+              <p>All Access Pass</p>
+            </div>
+            <div className={styles["ticket-id"]}>
+              <p>
+                Ticket ID: <span>24478457</span>
+              </p>
+            </div>
+          </div>
+          <div className={styles["website"]}>
+            <p>@devfestnagpur.in</p>
+          </div>
+          <div className={styles["sponsors"]}>
+            <div className={styles["sponsor"]}>
+              <GdgSvg></GdgSvg>
+            </div>
+            <div className={styles["sponsor"]}>
+              <img src={peerlist} alt="peerlist" />
+            </div>
+            <div className={styles["sponsor"]}>
+              <img src={orkes} alt="orkes" />
+            </div>
+            <div className={styles["sponsor"]}>
+              <img src={stream} alt="stream" />
+            </div>
+            <div className={styles["sponsor"]}>
+              <img src={moreficient} alt="moreficient" />
+            </div>
+          </div>
+          <div className={styles["monument"]}>
+            <MonumentSvg></MonumentSvg>
+          </div>
+          <div className={styles["monument1"]}>
+            <Monument1Svg></Monument1Svg>
           </div>
         </div>
-        <div className={styles["aside"]}></div>
+        <div
+          style={{
+            boxShadow:
+              "2px 2px 5px 0px rgba(26, 30, 38, 0.2), 0px -2px 5px 0px rgba(26, 30, 38, 0.2)",
+          }}
+          className={styles["aside"]}
+        >
+          <div className={styles["devfest-logo"]}>
+            <DevfestLightSvg></DevfestLightSvg>
+          </div>
+          <div className={styles["details"]}>
+            <div className={styles["detail"]}>
+              <h4>Event Date:</h4>
+              <p>
+                5<sup>th</sup> & 6<sup>th</sup> November, 2022
+              </p>
+            </div>
+            <div className={styles["detail"]}>
+              <h4>Venue:</h4>
+              <p>
+                Surest Bhat Auditorium, <br />
+                Nagpur
+              </p>
+            </div>
+            <div className={styles["detail"]}>
+              <h4>Reporting Time:</h4>
+              <p>11:00 AM IST</p>
+            </div>
+            <div className={styles["qrcode"]}>
+              <QRCode
+                size={80}
+                style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                value={
+                  "Hey this is devfest 2022 ticket for Bhavesh Chaudhari, payment id XXXXXXX"
+                }
+                viewBox={`0 0 80px 80px`}
+              />
+            </div>
+            <div className={styles["bridge"]}>
+              <BridgeSvg></BridgeSvg>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Ticket
+export default Ticket;
--- src/pages/ticket/TicketMain.js
+++ src/pages/ticket/TicketMain.js
@@ -1,15 +1,52 @@
-import React from 'react'
-import Ticket from "../../components/ticket/Ticket"
-import styles from "./TicketMain.module.css"
+import React, { useRef } from "react";
+import Ticket from "../../components/ticket/Ticket";
+import styles from "./TicketMain.module.css";
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
 
 const TicketMain = () => {
+  const printRef = React.useRef();
+
+  const handleDownloadPdf = async () => {
+    const element = printRef.current;
+    const canvas = await html2canvas(element);
+    const data = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF();
+    const imgProperties = pdf.getImageProperties(data);
+    const pageWidth = pdf.internal.pageSize.getWidth();
+    // const pageHeight = pdf.internal.pageSize.getHeight();
+
+    console.log(pdf.internal.pageSize.getHeight());
+
+    // const widthRatio = pageWidth / canvas.width;
+    // const heightRatio = pageHeight / canvas.height;
+    // const ratio = widthRatio > heightRatio ? heightRatio : widthRatio;
+
+    // const canvasWidth = canvas.width * ratio;
+    // const canvasHeight = canvas.height * ratio;
+
+    // const marginX = (pageWidth - canvasWidth) / 2;
+    // const marginY = (pageHeight - canvasHeight) / 2;
+
+    const pdfHeight = (imgProperties.height * pageWidth) / imgProperties.width;
+
+    pdf.addImage(data, "PNG", 0, 0, pageWidth, pdfHeight);
+    pdf.save(`DevFest Nagpur Ticket(Bhavesh Chaudhari).pdf`);
+  };
+
   return (
     <div className="layout-content">
       <div className={styles["container"]}>
-        <Ticket></Ticket>
+        <div ref={printRef} className={styles["ticket"]}>
+          <Ticket></Ticket>
+        </div>
+        <button type="button" onClick={handleDownloadPdf}>
+          Export as PDF
+        </button>
       </div>
     </div>
   );
-}
+};
 
-export default TicketMain
+export default TicketMain;
