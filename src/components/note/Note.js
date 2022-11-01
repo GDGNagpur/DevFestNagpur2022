b7831e18ae204fbd3fc283e726c0fa248788fa11
Bhavesh <bhic2030@gmail.com>
2022-11-01 13:09:01 +0530

    fix moreficient and note in dark mode

--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import styles from "./Note.module.css"
+import { useColorModeValue } from '../../hooks/useColorModeValue';
 
 const Note = () => {
   return (
     <div className={styles["container"]}>
-      <div className={styles["content"]}>
+      <div
+        style={{ background: useColorModeValue("#fde9e7", "white"), color: useColorModeValue("black", "black") }}
+        className={styles["content"]}
+      >
         <p>
-          <span>Important:</span>{" "}Last Min Passes are subject to availability on certain quantity . It might get refunded if needed.
+          <span>Important:</span> Last Min Passes are subject to availability on
+          certain quantity . It might get refunded if needed.
         </p>
       </div>
     </div>
