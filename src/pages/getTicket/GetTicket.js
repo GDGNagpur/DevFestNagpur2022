d86b025dcf6bb0c02d889437564179322e4e0941
Bhavesh <bhic2030@gmail.com>
2022-11-03 13:15:26 +0530

    add ticket notices

--- src/pages/getTicket/GetTicket.js
+++ src/pages/getTicket/GetTicket.js
@@ -1,10 +1,26 @@
 import React from "react";
 import TicketForm from "../../components/ticket/TicketForm";
+import styles from "./GetTicket.module.css"
+import { Link } from "react-router-dom";
 
 const GetTicket = () => {
   return (
     <div className="layout-content">
-      <TicketForm></TicketForm>
+      <div className={styles["container"]}>
+        <TicketForm></TicketForm>
+        <div className={styles["notices"]}>
+          <h4>Please note:</h4>
+          <ul>
+            <li>
+              You have to use the same email as you used for registration. If you still haven't registered then you can <Link to={"/"} >register here.</Link>
+            </li>
+            <li>
+              Those who have booked tickets on 3rd November, will be able to
+              download their passes on 4th November 12:00 PM.
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
