16288e146913cc3b31e0427419e117f95aafb23d
raghav1701 <54554575+raghav1701@users.noreply.github.com>
2022-11-06 08:43:34 +0530

    Update Note.js

--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -4,27 +4,6 @@ import { useColorModeValue } from '../../hooks/useColorModeValue';
 
 const Note = () => {
   return (
-    <div className={styles["container"]}>
-      <div
-        style={{
-          background: useColorModeValue("#fde9e7", "white"),
-          color: useColorModeValue("black", "black"),
-        }}
-        className={styles["content"]}
-      >
-        <p>
-          <span>Important:</span> For Bookings done on 4th and 5th November,
-          contact Raghav Agrawal on{" "}
-          <a
-            style={{ color: "#0f9d56", borderBottom: "1px underline #0f9d56" }}
-            href="tel:+917042347146"
-          >
-            7042347146
-          </a>{" "}
-          after making the purchase to receive your tickets.
-        </p>
-      </div>
-    </div>
   );
 }
 
